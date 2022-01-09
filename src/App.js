0d388ef3943e4308112e6cdfc9a4034f05c51629
Jaeyoon <sbjeong222@gmail.com>
2022-01-09 23:33:41 +0900

    map함수로 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 import React from "react";
 
+function Food({ name, picture }) {
+    return (
+        <div>
+            <h1>I like {name}</h1>;
+            <img src={picture} alt="음식사진" />
+        </div>
+    );
+}
+
+const foodLike = [
+    {
+        name: "Kimchi",
+        image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.IDrNtl4pG_zepTDko3IUhgHaHa%26pid%3DApi&f=1",
+    },
+    {
+        name: "Samgyeopsal",
+        image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.v0RiALj-qb0tA3TaocYnVAHaE7%26pid%3DApi&f=1",
+    },
+    {
+        name: "Bibimbap",
+        image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2S8riFnNgOm55O3kZjZiDgHaFG%26pid%3DApi&f=1",
+    },
+    {
+        name: "Dongasu",
+        image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.UpDCozCT4DXb6y8AvIzGgAHaE6%26pid%3DApi&f=1",
+    },
+    {
+        name: "Kimbap",
+        image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.XiO-APPrmYLstQXFFJDGrgHaE-%26pid%3DApi&f=1",
+    },
+];
+
 function App() {
-    return <div className="App" />;
+    return (
+        <div>
+            {foodLike.map((dish) => (
+                <Food name={dish.name} picture={dish.image} />
+            ))}
+        </div>
+    );
 }
 
 export default App;
