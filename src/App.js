cdc2f6845bf55fa7b922e2a04c2eb40e862f05c1
Jaeyoon <sbjeong222@gmail.com>
2022-01-11 21:19:01 +0900

    클래스형 컴포넌트 생명주기 함수

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("hello");
+    }
+
     state = {
         count: 0,
     };
@@ -13,7 +18,20 @@ class App extends React.Component {
         this.setState((current) => ({ count: current.count - 1 }));
     };
 
+    componentDidMount() {
+        console.log("component redered");
+    }
+
+    componentDidUpdate() {
+        console.log("I just updated");
+    }
+
+    componentWillUnmount() {
+        console.log("Goodbye, cruel world");
+    }
+
     render() {
+        console.log("I'm rendering");
         return (
             <div>
                 <h1>The number is : {this.state.count}</h1>
