002d3095947875aa9d4fe0bca682a767e2c6f3ac
Jaeyoon <sbjeong222@gmail.com>
2022-01-12 23:41:57 +0900

    movie 컴포넌트 만들기 완료

--- src/Movie.js
+++ src/Movie.js
@@ -1,16 +1,16 @@
 import React from "react";
 import propTypes from "prop-types";
 
-function Movie() {
-    return <h1></h1>;
+function Movie({ id, title, year, sumamry, poster }) {
+    return <h1>{title}</h1>;
 }
 
 Movie.propTypes = {
     id: propTypes.number.isRequired,
-    year: propTypes.string.isRequired,
+    year: propTypes.number.isRequired,
     title: propTypes.string.isRequired,
     summary: propTypes.string.isRequired,
-    proster: propTypes.string.isRequired,
+    poster: propTypes.string.isRequired,
 };
 
 export default Movie;
