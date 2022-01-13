717c609bc8ed988f4d5e87a899483ff91c354f55
Jaeyoon <sbjeong222@gmail.com>
2022-01-13 17:30:27 +0900

    poster image 띄우기

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,21 @@
 import React from "react";
 import propTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, title, year, sumamry, poster }) {
-    return <h1>{title}</h1>;
+function Movie({ title, year, summary, poster }) {
+    return (
+        <div class="movie">
+            <img src={poster} alt={title} title={title} />
+            <div class="movie__data">
+                <h3 class="movie__title">{title}</h3>
+                <h5 class="movie__year">{year}</h5>
+                <p class="movie__summary">{summary}</p>
+            </div>
+        </div>
+    );
 }
 
 Movie.propTypes = {
-    id: propTypes.number.isRequired,
     year: propTypes.number.isRequired,
     title: propTypes.string.isRequired,
     summary: propTypes.string.isRequired,
